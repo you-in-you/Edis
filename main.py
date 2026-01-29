2e614ba108e10238bafd12bba65bf95bdd028c2e
Reza <reza.you.in.you@gmail.com>
2026-01-29 13:31:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import socket
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Edis Protocol Initialized...")
+    print("Edis Protocol Initialized....")
 
 if __name__ == "__main__":
     start_server()
